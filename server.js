62c3dba6d70d8744fb4bca2d470a6ce6f4adf138
jlounsb1 <jefflounsbury@gmail.com>
2024-07-08 15:33:29 -0400

    setup pug server system

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const fs = require('fs');
+const pug = require('pug');
+
+app.set('view engine', 'pug');
+
+const logReq = function(req, res, next) {
+    console.log('Merp');
+    next();
+}
+
+app.use(logReq)
 
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`)
 });
 
 
-app.get("/", (req, res) => {
-    res.send("Test to make sure everythings working")
+app.use(express.static('public')) // This is to set up a basic static page as the home.
+app.use(express.static('./styles')) // use stylesheet in the apporopriate folder
+
+
+app.get('/', (req, res) => {
+    res.render(
+        'index',
+        {name:'Jeff'}
+    )
+})
+
+app.get('/about', (req, res) => {
+    res.render(
+        'about',
+        {name:'Radar'}
+    )
 })
