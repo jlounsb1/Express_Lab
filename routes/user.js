398744a1d605553f9adb59f0ccc41fe031610f40
jlounsb1 <jefflounsbury@gmail.com>
2024-07-08 15:36:55 -0400

    added user route from previous project

---
+++ routes/user.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router(); //For a router you need the Router() with the capital
+
+router.use((req, res, next) => {
+    console.log(`User router says hello at ${Date.now()}`)
+    next();
+})
+
+//for routs you have to set the base as /, then define what it is in the piece you export, usually server.js
+router
+    .route('/')
+    .get((req, res) => {
+        res.send(`Who Dis?`)
+    })
+    .post((req, res) => {
+        res.send(`Me Dis`)
+    })
+    .delete((req, res) => {
+        console.log(`Go away`)
+    });
+
+    router.get('./settings', (req, res) => {
+        res.send(`Get user Settings`)
+    });
+
+    module.exports = router;
+
+    //user page
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const fs = require('fs');
 const pug = require('pug');
+const userRoutes = require('./routes/user')
 
 app.set('view engine', 'pug');
 
@@ -17,7 +18,7 @@ app.listen(port, () => {
     console.log(`Server is listening at port ${port}`)
 });
 
-
+app.use('/user', userRoutes); // makes the user file a piece of middleware only used when on the user path as defined in this function
 app.use(express.static('public')) // This is to set up a basic static page as the home.
 app.use(express.static('./styles')) // use stylesheet in the apporopriate folder
 
