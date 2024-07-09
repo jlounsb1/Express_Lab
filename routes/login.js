c5d60a79cc30dfdfe6a73166a3151bbde48a3c73
jlounsb1 <jefflounsbury@gmail.com>
2024-07-09 14:51:05 -0400

    added ignorefile, if not a bit late

--- routes/login.js
+++ routes/login.js
@@ -9,5 +9,6 @@ router.post('/', (req, res) =>{
     res.send(`Your username is ${req.body.username}, and your password is ${req.body.password}.`);
     console.log(req.body.username, req.body.password)
 })
+//I was having trouble accessing the post data from the login form, found info in a youtube video.
 
 module.exports = router;
